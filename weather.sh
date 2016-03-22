c353654e7b885516fb9a0b2c7a7ac2fed0f94463
robby <robbystk@gmail.com>
2016-03-21 21:03:09 -0600

    Change to python script

--- weather.sh
+++ weather.sh
@@ -1,45 +1,47 @@
-#! /bin/bash
+#! /usr/bin/python2
+
+import os
 
 url='http://forecast.weather.gov/MapClick.php'
 
-function location_from_homedir()
-{
-    if [[ -f ~/.location ]]
-    then
-        . ~/.location
-    else
-        echo "no location file in home directory" >&2
-        exit 1
-    fi
-}
-
-function location_from_file()
-{
-    if [[ -f $location_file ]]
-    then
-        . $location_file
-    else
-        echo "file $location_file not found" >&2
-        location_from_homedir
-    fi
-}
-
-case $# in
-    0 )
-        # not given location file
-        location_from_homedir
-    ;;
-    1 )
-        # given location file
-        location_file=$1
-        location_from_file
-
-    ;;
-    * )
-        # wrong number of arguments
-        echo "Usage: $0 [location file]" >&2
-        exit 2
-    ;;
-esac
-
-curl -sGd "lat=${lat}&lon=${lon}&FcstType=digital" $url >forecast.html
+# function location_from_homedir()
+# {
+#     if [[ -f ~/.location ]]
+#     then
+#         . ~/.location
+#     else
+#         echo "no location file in home directory" >&2
+#         exit 1
+#     fi
+# }
+# 
+# function location_from_file()
+# {
+#     if [[ -f $location_file ]]
+#     then
+#         . $location_file
+#     else
+#         echo "file $location_file not found" >&2
+#         location_from_homedir
+#     fi
+# }
+# 
+# case $# in
+#     0 )
+#         # not given location file
+#         location_from_homedir
+#     ;;
+#     1 )
+#         # given location file
+#         location_file=$1
+#         location_from_file
+# 
+#     ;;
+#     * )
+#         # wrong number of arguments
+#         echo "Usage: $0 [location file]" >&2
+#         exit 2
+#     ;;
+# esac
+# 
+# curl -sGd "lat=${lat}&lon=${lon}&FcstType=digital" $url >forecast.html
