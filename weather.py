c2cafcc99cf09832ed02a45292179a3e0a83fb11
robby <robbystk@gmail.com>
2016-03-24 13:55:48 -0600

    Construct URL

--- weather.py
+++ weather.py
@@ -9,7 +9,7 @@ location_path="~/.location"
 def location_from_homedir():
     if isfile(expanduser(location_path)):
         with open(expanduser(location_path)) as f:
-            print "&".join(f.read().split("\n"))
+            return "&".join(f.read().split("\n"))
     else:
         print "no location file at " + location_path
 
@@ -23,13 +23,14 @@ def location_from_file(file):
 
 if len(argv) == 1:
     # not given location file
-    location_from_homedir()
+    data = location_from_homedir()
 elif len(argv) == 2:
     # given location file
-    location_from_file(argv[1])
+    data = location_from_file(argv[1])
 else:
     # wrong number of arguments
     print "Usage: $0 [location file]"
 
-# url = "http://forecast.weather.gov/MapClick.php?" + data + "&FcstType=digitalDWML"
+url="http://forecast.weather.gov/MapClick.php?"+data+"FcstType=digitalDWML"
+print url
 # forecast = urlopen(url).read()
