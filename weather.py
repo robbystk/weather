72d4c9af511283af48e0b187da93c420dc3e400d
Robby Stokoe <robbystk@gmail.com>
2018-11-17 16:50:35 -0700

    Special functions and a class for dealing with wind

--- weather.py
+++ weather.py
@@ -77,16 +77,68 @@ def get_series(forecast_root, variable, type_filter=None, transform=float):
 def rms(A):
     return sp.sqrt(sp.mean(A**2))
 
+class wind:
+    def __init__(self, speed, direction):
+        self.speed = speed
+        self.direction = direction
+
+    @property
+    def ns_comp(self):
+        return self.speed * sp.cos(self.direction * sp.pi / 180)
+
+    @property
+    def ew_comp(self):
+        return self.speed * sp.sin(self.direction * sp.pi / 180)
+
+    def mean_direction(self):
+        mean_ns = sp.mean(self.ns_comp)
+        mean_ew = sp.mean(self.ew_comp)
+        angle = sp.arctan2(mean_ew, mean_ns) * 180 / sp.pi
+        return angle
+
+def deg2str(angle):
+    # octant = sp.around(angle / 360 * 8) % 8
+    dodecant = sp.around(angle / 360 * 16) % 16
+    # dirs = {0: 'N', 1: 'NE', 2: 'E', 3: 'SE', 4: 'S', 5: 'SW', 6: 'W', 7: 'NW'}
+    dirs = {0: 'N', 1: 'NNE', 2: 'NE', 3: 'ENE',
+            4: 'E', 5: 'ESE', 6: 'SE', 7: 'SSE',
+            8: 'S', 9: 'SSW', 10: 'SW', 11: 'WSW',
+            12: 'W', 13: 'WNW', 14: 'NW', 15: 'NNW'}
+    return dirs[dodecant]
+
+def get_wind(forecast_root):
+    wind_speed = get_series(forecast_root, 'wind-speed')
+    wind_direction = get_series(forecast_root, 'direction')
+    return wind(wind_speed, wind_direction)
+
+def plot_wind(forecast_root):
+    wind = get_wind(forecast_root)
+    wind_NS = wind.speed * sp.cos(wind.direction * sp.pi / 180)
+    wind_EW = wind.speed * sp.sin(wind.direction * sp.pi / 180)
+
+    max_speed = sp.amax(wind.speed)
+
+    plt.plot(wind_EW[:49], wind_NS[:49])
+    plt.axis([-max_speed, max_speed, -max_speed, max_speed])
+    plt.xlabel('East/West')
+    plt.ylabel('North/South')
+    plt.show()
+
 def main():
     forecast = get_forecast(get_location_data(sys.argv))
     print("Precipitation: %.2f inches" %
             sp.sum(get_series(forecast, 'hourly-qpf')))
+    wind = get_wind(forecast)
+    print("Wind speed: %.2f mph" % rms(wind.speed))
+    print("Average wind direction: %s" % deg2str(wind.mean_direction()))
     temp = get_series(forecast, 'temperature', type_filter='hourly')
     print("max/min: %.0f/%.0f F" % (sp.amax(temp), sp.amin(temp)))
     print("dew point: %.0f F" %
             sp.mean(get_series(forecast, 'temperature', type_filter='dew point')))
     wind_chill = get_series(forecast, 'temperature', type_filter='wind chill')
     print("wind chill: %.0f/%.0f F" % (sp.amax(wind_chill), sp.amin(wind_chill)))
+
+    plot_wind(forecast)
 
 if __name__ == "__main__":
     main()
